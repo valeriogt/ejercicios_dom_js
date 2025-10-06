d12ddd92247b4f0db46b794c0b57b3be8661eb72
valeriogt <vgtvaleriogarcia@gmail.com>
2025-10-06 09:46:30 +0200

    Update ej1.js

---
+++ ej1.js
@@ -0,0 +1,22 @@
+// 1.1
+const button = document.querySelector('.showme');
+console.log(button);
+
+// 1.2
+const h1 = document.querySelector('#pillado');
+console.log(h1);
+
+// 1.3
+const paragraphs = document.querySelectorAll('p');
+console.log(paragraphs);
+
+// 1.4
+const pokemons = document.querySelectorAll('.pokemon');
+console.log(pokemons);
+
+// 1.5
+const testMeElements = document.querySelectorAll('[data-function="testMe"]');
+console.log(testMeElements);
+
+// 1.6
+console.log(testMeElements[2]);
